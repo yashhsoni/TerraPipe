943dc5a63cffaeac8d64da11b21ec61112395328
mishra-rd015 <154001355+mishra-rd015@users.noreply.github.com>
2025-04-09 00:49:50 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "4947feb5-b5f6-4284-acce-df1b262aedb0"
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = "rg-jenkins"
+  location = var.location
+}
+
+resource "azurerm_service_plan" "this" {
+  name                = "appserviceplan-jenkins"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  os_type             = "Linux"
+
+  sku_name = "B1"
+}
+
+resource "azurerm_linux_web_app" "this" {
+  name                = "reactapijenkinsrudram015"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  service_plan_id     = azurerm_service_plan.this.id  # Updated attribute
+
+  site_config {
+    always_on = true
+  }
+}
